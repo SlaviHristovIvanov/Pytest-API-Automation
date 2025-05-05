a599abab7c103ab4b5c85bad8b571d800e89a278
SlaviHristovIvanov <92828826+SlaviHristovIvanov@users.noreply.github.com>
2025-05-05 16:02:46 +0300

    Delete Pytest-API-Automation-main directory

--- Pytest-API-Automation-main/conftest.py
+++
@@ -1,8 +0,0 @@
-import requests
-import pytest
-
-@pytest.fixture
-def get_harry_potter_books():
-    url = 'https://www.googleapis.com/books/v1/volumes?q=harry+potter'
-    response = requests.get(url, timeout=5)
-    return response
